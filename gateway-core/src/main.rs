3ec3638611fb12a5fe336ee4fa7a7cdd58b1650e
George Waters <george@bakes.software>
2026-03-04 21:21:10 +0000

    feat: clap for args parsing

--- gateway-core/src/main.rs
+++ gateway-core/src/main.rs
@@ -1,13 +1,28 @@
-use std::env::var;
 use config::Config;
+use clap::Parser;
 
 mod config;
 
-fn main() {
-    let config_path = var("OPENAPI_CONFIG").unwrap();
-    let overlays_path = var("OVERLAYS_PATH").unwrap();
-    let overlays: Vec<&str> = overlays_path.split(",").collect();
-    let config = Config::parse(&config_path, &overlays);
+#[derive(Parser, Debug)]
+struct Args {
+    #[arg(long, env="OPENGATEWAY_CONFIG_PATH", help="The folder containing OpenGateway config")]
+    config_path: String,
+    #[arg(long, env="OPENGATEWAY_OPENAPI_SCHEMA", help="The path within the config folder to the OpenAPI schema")]
+    openapi_schema: String,
+    #[arg(
+        long="openapi-overlay", 
+        env="OPENGATEWAY_OPENAPI_OVERLAYS", 
+        value_delimiter=',',
+        help="The path within the config folder to any OpenAPI overlays, applied in order provided"
+        )]
+    openapi_overlays: Vec<String>,
+}
 
-    println!("{:#?}", config.extensions);
+fn main() {
+    let args = Args::parse();
+
+    match Config::parse(&args.config_path, &args.openapi_schema, &args.openapi_overlays) {
+        Ok(val) => println!("{:#?}", val.extensions),
+        Err(err) => eprintln!("Error parsing config: {}", err)
+    }
 }
